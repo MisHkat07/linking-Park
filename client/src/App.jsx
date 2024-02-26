4d86af88becefc5bda82d8dca6c7824119b37c04
MisHkat <mishkat606@gmail.com>
2024-02-26 18:03:44 +0600

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,15 @@ function App() {
   const [internalLinks, setInternalLinks] = useState([]);
   const [externalLinks, setExternalLinks] = useState([]);
   const [metaLinks, setMetaLinks] = useState([]);
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [searchPerformed, setSearchPerformed] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    setSearchPerformed(false);
+     setError("");
 
     try {
       const response = await axios.post(
@@ -21,10 +25,11 @@ function App() {
       setInternalLinks(response.data.internal);
       setExternalLinks(response.data.external);
       setMetaLinks(response.data.meta);
+      setSearchPerformed(true);
     } catch (error) {
+      setError("Not Found!")
       console.error("Error fetching links:", error);
     }
-    setWebsiteLink("");
     setLoading(false);
   };
 
@@ -37,86 +42,81 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        Website Link Scraper
-      </h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 px-4">
+      <h1 className="text-3xl font-bold mb-8">Website Link Scraper</h1>
       <form onSubmit={handleSubmit} className="mb-8">
         <div className="flex items-center justify-center mb-4 sm:mb-8">
           <input
             type="text"
             value={websiteLink}
             onChange={handleChange}
-            placeholder="Enter website link"
-            className="border border-gray-300 rounded-l-md px-4 py-2 w-full sm:w-auto focus:outline-none focus:border-blue-500"
+            placeholder="Enter webpage link"
+            className="border border-gray-300 rounded-l-md px-2 py-2 w-full sm:w-auto focus:outline-none focus:border-blue-500"
           />
           <button
             type="submit"
             className="bg-blue-500 text-white px-6 py-2 rounded-r-md ml-2 sm:ml-4 focus:outline-none"
           >
-            {loading ? "Scraping..." : "Scrape Links"}
+            {loading ? "Submitting.." : "Submit"}
           </button>
         </div>
+        <h4 className="text-red-500 overflow-hidden">{error}</h4>
       </form>
-      <div className="flex flex-col md:flex-row justify-center md:justify-between">
-        <div className="w-full md:w-1/3 border border-gray-300 rounded-md p-4 mb-4 md:mb-0">
-          <h2 className="text-xl font-semibold text-blue-500 mb-2">
-            Internal Links:
-          </h2>
-          <ul className="list-disc pl-6">
-            {internalLinks.map((link, index) => (
-              <li key={index} className="text-blue-500 overflow-hidden">
-                <a
-                  href={link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-block max-w-full"
-                >
-                  {index + 1}. {link}
-                </a>
-              </li>
-            ))}
-          </ul>
+      {searchPerformed && (
+        <div className="flex justify-between w-full">
+          <div className="w-full md:w-1/3 border border-gray-300 rounded-md p-4">
+            <h2 className="text-xl font-semibold mb-2">Internal Links:</h2>
+            <ul className="list-disc pl-6">
+              {internalLinks.map((link, index) => (
+                <li key={index} className="text-blue-500 overflow-hidden">
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block max-w-full"
+                  >
+                    {index + 1}. {link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full md:w-1/3 border border-gray-300 rounded-md p-4 mx-0 md:mx-4 mb-4 md:mb-0">
+            <h2 className="text-xl font-semibold mb-2">External Links:</h2>
+            <ul className="list-disc pl-6">
+              {externalLinks.map((link, index) => (
+                <li key={index} className="text-green-500 overflow-hidden">
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block max-w-full"
+                  >
+                    {index + 1}. {link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full md:w-1/3 border border-gray-300 rounded-md p-4">
+            <h2 className="text-xl font-semibold mb-2">Meta Links:</h2>
+            <ul className="list-disc pl-6">
+              {metaLinks.map((link, index) => (
+                <li key={index} className="text-purple-500 overflow-hidden">
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block max-w-full"
+                  >
+                    {index + 1}. {link}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="w-full md:w-1/3 border border-gray-300 rounded-md p-4 mx-0 md:mx-4 mb-4 md:mb-0">
-          <h2 className="text-xl font-semibold text-green-500 mb-2">
-            External Links:
-          </h2>
-          <ul className="list-disc pl-6">
-            {externalLinks.map((link, index) => (
-              <li key={index} className="text-green-500 overflow-hidden">
-                <a
-                  href={link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-block max-w-full"
-                >
-                  {index + 1}. {link}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="w-full md:w-1/3 border border-gray-300 rounded-md p-4">
-          <h2 className="text-xl font-semibold mb-2 text-purple-500">
-            Meta Links:
-          </h2>
-          <ul className="list-disc pl-6">
-            {metaLinks.map((link, index) => (
-              <li key={index} className="text-purple-500 overflow-hidden">
-                <a
-                  href={link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-block max-w-full"
-                >
-                  {index + 1}. {link}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
